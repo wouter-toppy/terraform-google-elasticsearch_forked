a12ad98a17ae1516a908d049c9f74260a41c37cc
George Lerma <george.lerma@sadasystems.com>
2019-04-07 12:35:00 -0500

    moar changes 2

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,7 @@ output "instances" {
 output "instance_group" {
   value = "${data.google_compute_region_instance_group.default.self_link}"
 }
+
+output "instance_group_self_link" {
+  value = "${module.node.region_instance_group}"
+}
