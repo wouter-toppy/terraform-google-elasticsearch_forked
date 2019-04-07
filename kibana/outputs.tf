e19ff197bd1e334aa1f5351d68b51ee9316a7c4e
George Lerma <george.lerma@sadasystems.com>
2019-04-07 12:29:31 -0500

    EN-5 - moar changes

--- kibana/outputs.tf
+++ kibana/outputs.tf
@@ -1,3 +1,8 @@
 output "instances" {
   value = "${data.google_compute_region_instance_group.default.instances}"
 }
+
+output "instance_group_self_link" {
+  value = "${module.kibana.region_instance_group}"
+}
+
